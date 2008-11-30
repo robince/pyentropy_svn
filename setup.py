f59caf4e44610cbfd68d580fa8ba1bfbfb41e800
robince <robince@a72bf258-8668-11dd-9ffd-cdf95c2cae98>
2008-11-30 21:04:13 +0000

    move pyentropy into subdir
    git-svn-id: http://pyentropy.googlecode.com/svn/trunk/pyentropy@36 a72bf258-8668-11dd-9ffd-cdf95c2cae98

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import pyentropy
+
+setup(name='pyentropy',
+      version=pyentropy.__version__,
+      description='Entropy and Information Theoretic Estimates',
+      author=pyentropy.__author__,
+      author_email='pyentropy@robince.net',
+      url='http://code.google.com/p/pyentropy',
+      packages=['pyentropy']
+      )
